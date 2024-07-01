08d994671d21619e3c22c974ee57b35b8a30baac
yuran1811 <trieuvanbd123@gmail.com>
2024-07-01 21:49:08 +0700

    perf: ⚡ adjust cache-control for better response

--- src/routes/layout.ts
+++ src/routes/layout.ts
@@ -2,7 +2,7 @@ import type { RequestHandler } from '@builder.io/qwik-city';
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   cacheControl({
-    staleWhileRevalidate: 60 * 60 * 24,
-    maxAge: 5,
+    staleWhileRevalidate: 60 * 60 * 24 * 14,
+    maxAge: 60 * 30,
   });
 };
